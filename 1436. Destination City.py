29e1ac54bca9de036211280e464cf3069ad0a52e
Aylin <gasanovaa144@gmail.com>
2024-03-23 02:56:17 +0400

    class Solution(object):     def destCity(self, paths):         cities = set()
            for path in paths:
                cities.add(path[0])
    
            for path in paths:
                dest = path[1]
                if dest not in cities:
                    return dest
    
            return ""

---
+++ 1436. Destination City.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def destCity(self, paths):
+        cities = set()
+
+        for path in paths:
+            cities.add(path[0])
+
+        for path in paths:
+            dest = path[1]
+            if dest not in cities:
+                return dest
+
+        return ""
