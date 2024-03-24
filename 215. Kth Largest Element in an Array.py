11dd5ec070a57f74e8ab7cb21705a2dc1f7ae30d
Aylin <gasanovaa144@gmail.com>
2024-03-25 00:37:08 +0400

    class Solution(object):     def findKthLargest(self, nums, k):         k = len(nums) - 1
            def quickSelect(l, r):
                pivot, p = nums[r], l
                for i in range(l, r):
                    if nums[i] <= pivot:
                        nums[p], nums[i] = nums[i], nums[p]
                        p += 1
                nums[p], nums[r] = nums[r], nums[p]
    
                if p > k:
                    return quickSelect(l, p - 1)
                elif p < k:
                    return quickSelect(p + 1, r)
                else:
                    return nums[p]
    
            return quickSelect(0, len(nums) - 1)

---
+++ 215. Kth Largest Element in an Array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        k = len(nums) - 1
+
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
