e43eb1be2fd06d5ef0db5e268ed7ce56673b49e5
Aylin <gasanovaa144@gmail.com>
2024-03-24 23:03:30 +0400

    class Solution(object):     def hasCycle(self, head):         slow = head         fast = head         while fast and fast.next:             slow = slow.next             fast = fast.next.next             if slow == fast:                 return True         return False

---
+++ 141. Linked List Cycle.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
