a98d41fbddd053fb34f2d62a74f3c7d5b3845373
Aylin <gasanovaa144@gmail.com>
2024-03-24 23:29:10 +0400

    class Solution(object):     def isPalindrome(self, head):         fast = head         slow = head         while fast and fast.next:             fast = fast.next.next             slow = slow.next         prev = None         while slow:             tmp = slow.next             slow.next = prev             prev = slow             slow = tmp
            left, right = head, prev
            while right:
                if left.val != right.val:
                    return False
                left = left.next
                right = right.next
            return True

---
+++ 234. Palindrome Linked List.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
